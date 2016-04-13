f17e68b8f0fedd7403dc35ab16307738f2e36e67
Awais <awais.qureshi@arbisoft.com>
2016-04-13 19:04:01 +0500

    Adding backbone code. ECOM-3199

--- lms/static/js/learner_dashboard/program_list_factory.js
+++ lms/static/js/learner_dashboard/program_list_factory.js
@@ -3,17 +3,38 @@
 
     define([
         'js/learner_dashboard/views/collection_list_view',
-//        'js/learner_dashboard/views/sidebar_view',
+        'js/learner_dashboard/views/sidebar_view',
         'js/learner_dashboard/views/program_card_view',
-        'js/learner_dashboard/collections/program_collection'
+        'js/learner_dashboard/collections/program_collection',
+        'js/learner_dashboard/views/certificate_view',
+        'js/learner_dashboard/collections/certificate_collection'
     ],
-    function (CollectionListView, ProgramCardView, ProgramCollection) {
+    function (
+            CollectionListView,
+            SidebarView,
+            ProgramCardView,
+            ProgramCollection,
+            CertificateView,
+            CertificateCollection
+        ) {
         return function (options) {
             new CollectionListView({
                 el: '.program-cards-container',
                 childView: ProgramCardView,
                 collection: new ProgramCollection(options.programsData)
             }).render();
+
+            new SidebarView({
+                el: '.sidebar',
+                context: options
+            }).render();
+
+            new CollectionListView({
+                el: '.certificate',
+                childView: CertificateView,
+                collection: new CertificateCollection(options.certificatesData)
+            }).render();
+
         };
     });
 }).call(this, define || RequireJS.define);
