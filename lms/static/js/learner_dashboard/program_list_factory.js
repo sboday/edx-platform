6acfa7e351a8173d4766930aac8f5b0c91d6c8a7
Awais <awais.qureshi@arbisoft.com>
2016-04-13 15:54:30 +0500

    Adding backbone code. ECOM-3199

--- lms/static/js/learner_dashboard/program_list_factory.js
+++ lms/static/js/learner_dashboard/program_list_factory.js
@@ -3,11 +3,11 @@
 
     define([
         'js/learner_dashboard/views/collection_list_view',
-        'js/learner_dashboard/views/sidebar_view',
+//        'js/learner_dashboard/views/sidebar_view',
         'js/learner_dashboard/views/program_card_view',
         'js/learner_dashboard/collections/program_collection'
     ],
-    function (CollectionListView, SidebarView, ProgramCardView, ProgramCollection) {
+    function (CollectionListView, ProgramCardView, ProgramCollection) {
         return function (options) {
             new CollectionListView({
                 el: '.program-cards-container',
