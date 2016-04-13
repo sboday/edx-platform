31882cbd6b2d26e2d32d2fbd9e3f9cfbbc54c8e2
Usman Khalid <2200617@gmail.com>
2016-04-13 19:08:08 +0500

    only run common-requirejs

--- pavelib/utils/envs.py
+++ pavelib/utils/envs.py
@@ -121,8 +121,8 @@ class Env(object):
         # REPO_ROOT / 'lms/static/karma_lms_coffee.conf.js',
         # REPO_ROOT / 'cms/static/karma_cms.conf.js',
         # REPO_ROOT / 'cms/static/karma_cms_squire.conf.js',
-        REPO_ROOT / 'common/lib/xmodule/xmodule/js/karma_xmodule.conf.js',
-        REPO_ROOT / 'common/static/karma_common.conf.js',
+        # REPO_ROOT / 'common/lib/xmodule/xmodule/js/karma_xmodule.conf.js',
+        # REPO_ROOT / 'common/static/karma_common.conf.js',
         REPO_ROOT / 'common/static/karma_common_requirejs.conf.js',
     ]
 
@@ -131,8 +131,8 @@ class Env(object):
         # 'lms-coffee',
         # 'cms',
         # 'cms-squire',
-        'xmodule',
-        'common',
+        # 'xmodule',
+        # 'common',
         'common-requirejs'
     ]
 
